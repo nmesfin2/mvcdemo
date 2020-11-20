c11b4b1f1f52969340009a158e249f23e8912bbc
nat <nmexodus870@gmail.com>
2020-11-20 16:50:50 +0000

    home view

---
+++ mvcdemo/src/main/java/com/mvcdemo/config/RootConfig.java
@@ -0,0 +1,8 @@
+package com.mvcdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+// services repositories
+}
--- mvcdemo/src/main/webapp/WEB-INF/views/home.jsp
+++ mvcdemo/src/main/webapp/WEB-INF/views/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	Hello From Spring Core
 </body>
 </html>
